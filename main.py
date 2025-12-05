985f094d2744b485c8aef6a96a9ec6189e18b68d
Daksh Dubey <jaydubey0039@gmail.com>
2025-12-05 15:29:17 +0530

    the code is updated for data

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from pipelines.hybrid_cnn import pipe
 if __name__ == "__main__":
     # Define the paths for your data
     # IMPORTANT: Create a 'data' folder and put your image class folders inside it.
-    INPUT_DATA_PATH = "C:\\Users\\jaydu\\OneDrive\\Desktop\\python_projects\\Mlops1\\Main dataset"
-    OUTPUT_DATA_PATH = "C:\\Users\\jaydu\\OneDrive\\Desktop\\python_projects\\Mlops1\\output" # A folder to store the split data
+    INPUT_DATA_PATH = r"C:\Users\jaydu\OneDrive\Desktop\python_projects\Mlops3\Main dataset"
+    OUTPUT_DATA_PATH = r"C:\Users\jaydu\OneDrive\Desktop\python_projects\Mlops3\output" # A folder to store the split data
     pipe(INPUT_DATA_PATH,OUTPUT_DATA_PATH)
